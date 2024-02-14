4ba0b6ce1dbe90ffd13cc4c687bfe92ee6d65815
dwc808 <dc.1989@gmail.com>
2024-02-14 18:18:36 -0500

    Updated queries to show values rather than FKs

--- FrontendSQL.sql
+++ FrontendSQL.sql
@@ -16,8 +16,8 @@ VALUES ({species_input}, {class_input}, {fListed_input}, {expected_input});
 SELECT * FROM Locations;
 
 -- Query for Creating new Location --
-INSERT INTO Locations (county, town, inPark, parkName)
-VALUES ({county_input}, {town_input}, {inPark}, {parkName});
+INSERT INTO Locations (parkName)
+VALUES ({park_input});
 
 ----------------------------------------
 --            Observers               --
@@ -47,16 +47,22 @@ SELECT species FROM Animals;
 INSERT INTO ObserverAnimals (observerID, animalID, expertiseLevel, timeofDayPreference)
 VALUES ({observer_choice}, {animal_choice}, {expertise_input}, {time_input});
 
--- ADD CITATION --
+-- Query below modelled after example Delete from exploration
+-- https://canvas.oregonstate.edu/courses/1946034/pages/exploration-database-application-design?module_item_id=23809325
+
 -- Query for deleting ObserverAnimal --
 DELETE FROM ObserverAnimals
 WHERE observerID = {chosen_observer};
 
--- FINISH TOMORROW - will want observer name and animal species, joined to expertise? -- 
 -- Query to select ObserverAnimals' data for update --
-SELECT 
+SELECT ObserverAnimals.observerAnimalID, Observers.name, Animals.species, ObserverAnimals.expertiseLevel, ObserverAnimals.timeofDayPreference
+FROM ObserverAnimals
+JOIN Observers ON ObserverAnimals.observerID = Observers.observerID
+JOIN Animals ON ObserverAnimals.animalID = Animals.animalID;
 
--- Citation --
+-- Query below modelled after example Update from exploration
+-- https://canvas.oregonstate.edu/courses/1946034/pages/exploration-database-application-design?module_item_id=23809325
+
 -- Query to update the ObserverAnimal --
 UPDATE ObserverAnimals
     SET expertiseLevel = {expertise_input}, timeofDayPreference = {time_input}
@@ -67,11 +73,31 @@ UPDATE ObserverAnimals
 ----------------------------------------
 
 -- Query for viewing Sightings --
-SELECT * FROM Sightings;
+SELECT Sightings.sightingID, Observers.name, Animals.species, Locations.parkName, Sightings.sightingTime, Sightings.sightingDate, Sightings.weather
+FROM Sightings
+JOIN Observers ON Sightings.observerID = Observers.observerID
+JOIN Animals ON Sightings.animalID = Animals.animalID
+JOIN Locations ON Sightings.locationID = Locations.locationID;
 
--- Query for filling Locations Dropdown --
-SELECT town FROM Locations;
+-- Query for filling Park Name Dropdown --
+SELECT parkName FROM Locations;
 
--- NOTE - WHAT WILL WE UPDATE FOR SIGHTINGS? --
+-- Query for filling Observer Name Dropdown --
+SELECT name FROM Observers;
+
+-- Query for filling Species Dropdown --
+SELECT species FROM Animals;
+
+-- Query for creating new Sighting --
+
+INSERT INTO Sightings (locationID, observerID, animalID, sightingTime, sightingDate, weather)
+VALUES ({location_input}, {observer_input}, {animal_input}, {time_input}, {date_input}, {weather_input});
+
+-- Query for updating Sighting -- 
+
+UPDATE Sightings    
+    SET locationID = {location_input}, observerID = {observer_input}, animalID = {animal_input}, 
+    sightingTime = {time_input}, sightingDate = {date_input}, weather = {weather_input}
+    WHERE sightingID = {Sighting_ID_from_update}
 
 
