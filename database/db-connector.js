cab29ec3d378902f9d1b2044149096f45bab58df
Adam Leatherman <aleatherman1124@gmail.com>
2024-02-27 20:18:52 -0500

    Sanitize db-connector.js

--- database/db-connector.js
+++ database/db-connector.js
@@ -7,8 +7,8 @@ var mysql = require('mysql')
 var pool = mysql.createPool({
     connectionLimit : 10,
     host            : 'classmysql.engr.oregonstate.edu',
-    user            : 'cs340_leathead',
-    password        : '7793',
+    user            : '',
+    password        : '',
     database        : 'cs340_leathead'
 })
 
