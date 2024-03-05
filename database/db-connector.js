eee7a0722956095a1cb0e6efcc14dc89238a3f5d
Adam Leatherman <leathead@flip4.engr.oregonstate.edu>
2024-03-05 14:04:43 -0800

    Changed pw, sanitized input

--- database/db-connector.js
+++ database/db-connector.js
@@ -2,13 +2,14 @@
 
 // Get an instance of mysql we can use in the app
 var mysql = require('mysql')
+require('dotenv').config(); // Load environment variables from .env file
 
 // Create a 'connection pool' using the provided credentials
 var pool = mysql.createPool({
     connectionLimit : 10,
     host            : 'classmysql.engr.oregonstate.edu',
     user            : 'cs340_leathead',
-    password        : '7793',
+    password        : process.env.DB_PASSWORD,
     database        : 'cs340_leathead'
 })
 
