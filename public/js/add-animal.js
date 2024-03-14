21acbdf2935b89c104b60fea3d47d33d9df3b711
Adam Leatherman <leathead@flip4.engr.oregonstate.edu>
2024-03-14 08:31:08 -0700

    Cleaned up Animals pt2

--- public/js/add-animal.js
+++ public/js/add-animal.js
@@ -1,16 +1,6 @@
 //add-animal.js
 // Get the objects we need to modify
-let showform = document.getElementById('show-create');
-
 let addAnimalForm = document.getElementById("add-animal-form-ajax");
-
-showform.addEventListener('click', () => {
-    
-    if (addAnimalForm.style.display === 'none') {
-        addAnimalForm.style.display = 'block';
-    }
-})
-
 
 // Modify the objects we need
 addAnimalForm.addEventListener("submit", function (e) {
@@ -125,13 +115,6 @@ addRowToTable = (data) => {
     // Add the row to the table
     currentTable.appendChild(row);
     
-    //Update the drop down menu with newly created/edited rows
-    let selectMenu = document.getElementById("animalSelect");
-    let option = document.createElement("option");
-    option.text = newRow.species;
-    option.value = newRow.id;
-    selectMenu.add(option);
-
     // Refresh the page
     window.location.reload();
 }
